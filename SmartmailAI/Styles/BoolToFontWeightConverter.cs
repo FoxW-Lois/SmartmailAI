3ec5261b1334564be61ec2f6032ac709a68aef83
FoxW-Lois <whaterfoxgaming@gmail.com>
2026-01-05 00:09:24 +0100

    Add yellow star for email with IsStarred = true + Sendername and Subject FontWeights = Bold for IsRead = false

---
+++ SmartmailAI/Styles/BoolToFontWeightConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml.Data;
+
+namespace SmartmailAI.Styles;
+
+public partial class BoolToFontWeightConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, string language)
+	{
+		if (value is bool isRead)
+		{
+			return isRead ? FontWeights.Normal : FontWeights.Bold;
+		}
+
+		return FontWeights.Normal;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
