c9ddc247891d7149648f6321315ad91ac61f33de
FoxW-Lois <whaterfoxgaming@gmail.com>
2026-02-04 12:50:21 +0100

    Continuation of the previous commit

---
+++ SmartmailAI.Core/Services/Addresses/GmailApiService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using SmartmailAI.Core.Contracts.Services.Addresses;
+
+namespace SmartmailAI.Core.Services.Addresses;
+
+public class GmailApiService : IGmailApiService
+{
+	public async Task<string> GetEmailAddressAsync(UserCredential credential)
+	{
+		var service = new GmailService(new BaseClientService.Initializer
+		{
+			HttpClientInitializer = credential,
+			ApplicationName = "MailOAuthTester"
+		});
+
+		var profile = await service.Users.GetProfile("me").ExecuteAsync();
+		return profile.EmailAddress;
+	}
+}
